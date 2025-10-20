a931e2380516e75fd7adcb742d106185e43e6c2c
luo-jia1314 <3069142301@qq.com>
2025-10-20 11:53:51 +0800

    Site updated: 2025-10-20 11:53:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/18/如何用github创建一个属于自己的blog/"];function toRandomPost(){
+var posts=["2025/10/18/如何用github创建一个属于自己的blog/","2025/10/20/大学生如何去找实习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
